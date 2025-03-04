576f1ba0ea7b5e6957caf3895058a52ef3a7cfea
Javier Moya <168685523+javiermoya2504@users.noreply.github.com>
2025-03-04 11:27:22 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,12 +1,7 @@
 from flask import Flask
 
+# Application initializations
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+# settings
+app.secret_key = "mysecretkey"
